6d14699080182963be6842954cc33ed856c0700b
Ubuntu <ubuntu@ip-172-31-31-176.ec2.internal>
2025-02-16 00:45:55 +0000

    replaced sqlite with RDS postgres database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hustle',
+        'USER': 'ryancheney',
+        'PASSWORD': '&bIi&Z!Rf&T5##*A',
+        'HOST': 'hustle-db.cz22oqoiw6f1.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
