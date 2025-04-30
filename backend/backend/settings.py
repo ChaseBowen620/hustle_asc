1f329085c3644b91e4f4b5f181533e2f586ffefc
Ubuntu <ubuntu@ip-172-31-7-57.us-west-1.compute.internal>
2025-04-30 18:42:57 +0000

    Initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,24 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'hustle'),
-        'USER': os.environ.get('DB_USER', 'ryancheney'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', '&bIi&Z!Rf&T5##*A'),
-        'HOST': os.environ.get('DB_HOST', 'hustle-db.cz22oqoiw6f1.us-east-1.rds.amazonaws.com'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
